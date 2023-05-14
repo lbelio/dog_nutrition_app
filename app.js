0fb1493cc608ff242bfa203756ab27de39663c8d
lbelio <114439501+lbelio@users.noreply.github.com>
2023-05-14 22:17:16 +0100

    functioning autosuggest

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Dog breed selector
 
-const breedApiUrl = "https://api.thedogapi.com/v1/breeds";
+const breedApiUrl = "https://api.thedogapi.com/v1/breeds/";
 const headers = {
   "x-api-key":
     "live_4atmgORNWxUZUQaK0V2KGt3AkyxjisK9TXDVhqSbRvoxWyezCu7blzvxtA97e4JE",
@@ -12,22 +12,68 @@ const breedSuggestions = document.getElementById("breedSuggestions");
 
 //Execute function on keyup
 
-breedInput.addEventListener("keyup", () => {
-  const breedUserInput = breedInput.value;
-});
+breedInput.addEventListener("keyup", getBreeds);
+
+// get matching breed names
+// async function getBreeds() {
+//   try {
+//     const response = await fetch(breedApiUrl, { headers });
+//     const data = await response.json();
+//     let breedUserInput = breedInput.value;
+//     if (breedUserInput.length > 1) {
+//       //filter matching breeds using user input
+//       let matchingBreeds = data.filter((breed) =>
+//         breed.name.toLowerCase().startsWith(breedUserInput.toLowerCase())
+//       );
+
+//       // extract breed names from the data array and clear previous suggestions before appending new ones
+//       let breedNames = matchingBreeds.map((breed) => breed.name);
+//       breedSuggestions.innerHTML = "";
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 async function getBreeds() {
   try {
     const response = await fetch(breedApiUrl, { headers });
     const data = await response.json();
-    if (breedUserInput.length > 2) {
-    }
-    //funtion here
+
+    breedInput.addEventListener("keyup", (e) => {
+      let breedUserInput = breedInput.value.trim().toLowerCase();
+      let suggestionList = document.querySelector(".list");
+      suggestionList.innerHTML = "";
+      if (breedUserInput.length > 1) {
+        let matchingBreeds = data.filter((breed) =>
+          breed.name.toLowerCase().startsWith(breedUserInput)
+        );
+        let breedNames = matchingBreeds.map((breed) => breed.name);
+        // console.log(breedNames);
+        breedNames.forEach((name) => {
+          let listItem = document.createElement("li");
+          listItem.classList.add("list-items");
+          listItem.style.cursor = "pointer";
+          listItem.textContent = name;
+          listItem.addEventListener("click", () => {
+            breedInput.value = name;
+            suggestionList.innerHTML = "";
+          });
+          suggestionList.appendChild(listItem);
+         // console.log(listItem);
+        });
+      }
+    });
   } catch (error) {
-    console.log(error);
+    console.log("error");
   }
 }
-const breedFetch = fetch("https://api.thedogapi.com/v1/breeds");
+
+function displayNames(value) {
+  breedUserInput;
+}
+
+getBreeds();
 
 //Dog weight user input
 
@@ -39,13 +85,12 @@ function dogWeight() {
 //const = dailyCalories = RER * life stage factor * activity factor
 // const restEnergyRequirement = 70 * body weight ^ 0.75
 
-const activityHover = document.querySelector("#activityLevel");
+const activitySelector = document.querySelector("#activityLevel");
 
-function activityExplanation() {
-  activityHover.addEventListener("hover");
-}
-
+//Submit modal activation
 const submitButton = document.getElementById("submit");
-
 const modal = document.querySelector(".modal");
-submitButton.addEventListener("click", modal.classList.add(".modalActivated"));
+submitButton.addEventListener("click", () => {
+  modal.classList.add(".modalActivated");
+  console.log("added");
+});
