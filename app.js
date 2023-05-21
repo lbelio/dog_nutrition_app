b021a5c4307e9237cb4685182cb35c852db09da1
lbelio <114439501+lbelio@users.noreply.github.com>
2023-05-21 03:00:17 +0100

    completed autosuggest and other smaller bits

--- app.js
+++ app.js
@@ -14,26 +14,7 @@ const breedSuggestions = document.getElementById("breedSuggestions");
 
 breedInput.addEventListener("keyup", getBreeds);
 
-// get matching breed names
-// async function getBreeds() {
-//   try {
-//     const response = await fetch(breedApiUrl, { headers });
-//     const data = await response.json();
-//     let breedUserInput = breedInput.value;
-//     if (breedUserInput.length > 1) {
-//       //filter matching breeds using user input
-//       let matchingBreeds = data.filter((breed) =>
-//         breed.name.toLowerCase().startsWith(breedUserInput.toLowerCase())
-//       );
-
-//       // extract breed names from the data array and clear previous suggestions before appending new ones
-//       let breedNames = matchingBreeds.map((breed) => breed.name);
-//       breedSuggestions.innerHTML = "";
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+//Breed API access and autosuggest function
 
 async function getBreeds() {
   try {
@@ -49,18 +30,17 @@ async function getBreeds() {
           breed.name.toLowerCase().startsWith(breedUserInput)
         );
         let breedNames = matchingBreeds.map((breed) => breed.name);
-        // console.log(breedNames);
         breedNames.forEach((name) => {
           let listItem = document.createElement("li");
           listItem.classList.add("list-items");
           listItem.style.cursor = "pointer";
           listItem.textContent = name;
+
           listItem.addEventListener("click", () => {
             breedInput.value = name;
             suggestionList.innerHTML = "";
           });
           suggestionList.appendChild(listItem);
-         // console.log(listItem);
         });
       }
     });
@@ -69,18 +49,10 @@ async function getBreeds() {
   }
 }
 
-function displayNames(value) {
-  breedUserInput;
-}
-
-getBreeds();
-
 //Dog weight user input
 
-function dogWeight() {
-  const dogWeightInput = document.getElementById("dogWeight");
-  const data = dogWeightInput.value;
-}
+const dogWeightInput = document.getElementById("dogWeight");
+const weight = dogWeightInput.value;
 
 //const = dailyCalories = RER * life stage factor * activity factor
 // const restEnergyRequirement = 70 * body weight ^ 0.75
@@ -89,8 +61,16 @@ const activitySelector = document.querySelector("#activityLevel");
 
 //Submit modal activation
 const submitButton = document.getElementById("submit");
-const modal = document.querySelector(".modal");
-submitButton.addEventListener("click", () => {
-  modal.classList.add(".modalActivated");
-  console.log("added");
-});
+
+
+
+
+//Calculation of the final value
+
+//Resting energy requirement
+let RER = 30 * weight + 70
+
+// Total Daily Energy Expenditure 
+let TDEE = 
+
+
